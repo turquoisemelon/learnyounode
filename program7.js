abdc720167a0761031421c3d90a2a25fef5e0b36
meltem kilic <melltemkilic@gmail.com>
2017-03-31 17:59:45 -0400

    exercise-7 solved

--- program7.js
+++ program7.js
@@ -1,4 +1,14 @@
 const http = require('http');
-const bufferList = require('bl');
+const bl = require('bl');
 
 let url = process.argv[2];
+
+http.get(url, function callback (response) {
+  response.pipe(bl(function(err, data) {
+    if (err) {
+    return console.error('There was an error:', error);
+    }
+  console.log(data.toString().length);
+  console.log(data.toString());
+}))
+});
